593c9dd4a933ae6a2a33edcabd4ede53819587a1
ssoorajr <50820025+ssoorajr@users.noreply.github.com>
2019-10-11 11:29:39 +0530

    Update get_external_link_zonari_async.py

--- plugins/get_external_link_zonari_async.py
+++ plugins/get_external_link_zonari_async.py
@@ -64,7 +64,7 @@ async def get_link(bot, update):
         reply_message = update
     else:
         return False
-    if str(update.from_user.id) in Config.AUTH_USERS:
+    if str(update.from_user.id) in Config.BANNED_USERS:
         await bot.send_message(
             chat_id=update.chat.id,
             text=Translation.ABUSIVE_USERS,
